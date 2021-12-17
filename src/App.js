5869dc9e673290cea32dadac27d1856b6bed584e
unknown <saba4inasaridze@gmail.com>
2021-12-17 10:00:39 +0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* app name: Weatherma */}
+      {/* general current weather: Location, temperature, weatherIcon, weatherText */}
+      {/* details about current weather: Humidity, wind speed, Visibility, Pressure */}
+    </div>
+  );
+}
+
+export default App;
